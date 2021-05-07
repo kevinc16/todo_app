6e2935a1f564472140bda60064e6f944d7d96da1
k9lvn <kevinchen828@hotmail.com>
2021-05-07 19:09:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+function Tile(props) {
+  const [done, setDone] = useState(props.todo.isCompleted);
+
+  const addButtons = () => {
+    return (
+      <>
+        <button
+          onClick={() => {
+            setDone(true);
+            props.todo.isCompleted = true;
+          }}
+        >
+          done
+        </button>
+        <button
+          onClick={() => {
+            props.remove(props.index);
+          }}
+        >
+          x
+        </button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      {done ? (
+        <div className="tile done">
+          <p>{props.todo.text}</p>
+          {addButtons()}
+        </div>
+      ) : (
+        <div className="tile">
+          <p>{props.todo.text}</p>
+          {addButtons()}
+        </div>
+      )}
+    </>
+  );
+}
+
+function Display(props) {
+  console.log(props.tiles);
+  return (
+    <div>
+      {props.tiles.map((element, index) => {
+        return (
+          <Tile
+            key={index}
+            todo={element}
+            index={index}
+            remove={props.remove}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Input(props) {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type={props.type}
+      />
+      <input
+        type="submit"
+        value="Add"
+        onClick={() => {
+          if (value) props.addTodo({ text: value, isCompleted: false });
+          setValue("");
+        }}
+      ></input>
+    </>
+  );
+}
 
 function App() {
+  const [tiles, setTiles] = useState([{ text: "example", isCompleted: false }]);
+
+  const removeTile = (index) => {
+    const newTodos = [...tiles];
+    newTodos.splice(index, 1);
+    setTiles(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Todo</h1>
+      <div className="label-text">
+        <label>New todo:</label>
+        <Input
+          type={"text"}
+          addTodo={(newVal) => {
+            setTiles([...tiles, newVal]);
+          }}
+        />
+      </div>
+      {/* <label className="label-text">New todo:</label>
+      <Input
+        type={"text"}
+        addTodo={(newVal) => {
+          setTiles([...tiles, newVal]);
+        }}
+      /> */}
+      <Display tiles={tiles} remove={removeTile} />
     </div>
   );
 }
